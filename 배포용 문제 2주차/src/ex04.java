2fe35e088e18486ecdd7253a8842f4099067f1f0
jw00n <jw00n0212@gmail.com>
2021-12-01 11:20:16 +0900

    업데이트

--- 배포용 문제 2주차/src/ex04.java
+++ 배포용 문제 2주차/src/ex04.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 //로또는 숫자 6개를 뽑는다.
@@ -8,8 +9,9 @@ public class ex04 {
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = random.nextInt(100);// 랜덤 숫자 지정
+		}
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println("행운의 숫자: " + arr[i]);
 		}
-
 	}
 }
