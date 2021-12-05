ce90b82360e2231303e3db07811bd3406aea65b0
dayul <dayul@DESKTOP-G73CG1A>
2021-12-06 01:29:18 +0900

    배포 문제 - 합병, middle, 하샤드 수

--- 배포용 문제/배포용 문제 3주차/src/ex1203_02.java
+++ 배포용 문제/배포용 문제 3주차/src/ex1203_02.java
@@ -1,9 +1,51 @@
+import java.util.Arrays;
+
 
 public class ex1203_02 {
-
+//합병 후 오름차순으로 정렬하여 반환
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int A[] = {1,3,5,7,9,11,13,15,17,19};
+		int B[] = {2,4,6,8,10,12,14,16,18,20};
+		
+		int Merge[]= mergeArray(A, B);
+		System.out.println("Merge : "+Arrays.toString(Merge));
 
 	}
+	
+	//1. 오름차순으로 정렬된 배열을 합친다.
+	public static int[] mergeArray(int[] A, int[] B) {
+		int[] arr = new int[A.length+B.length];
+		
+		int h= A.length-1;
+		int m= B.length-1;
+		
+		int i=0, j=0, k=0;
+
+		while(i<=h && j<=m) {
+			if(A[i]<B[j]) {
+				arr[k]=A[i];
+				i++;
+			}
+			else {
+				arr[k]=B[j];
+				j++;
+			}
+			k++;
+		}
+		
+		if(i>h) {
+			arr[k]=B[m];
+		}else if(j>h) {
+			arr[k]=A[h];
+		}
+		
+	
+		
+	
+
+		return arr;
+	}
+	
+	
 
 }
